612191d8c42ca19764a15fef9a023308e0ddc09b
vinod.kamasani@gmail.com <vinod.kamasani@gmail.com>
2025-11-05 15:54:46 +0530

    single container

--- Shopping/Shopping.Client/obj/Release/net8.0/Shopping.Client.AssemblyInfo.cs
+++ Shopping/Shopping.Client/obj/Release/net8.0/Shopping.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shopping.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c7e839165a1db34da08e674e37b8c16ebca596a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shopping.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shopping.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
